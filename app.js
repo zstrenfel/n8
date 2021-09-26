f3dcdea1f44730166dbbcd82741fe99e185f17cf
Zach Strenfel <zwstrenfel@gmail.com>
2021-09-25 20:01:09 -0400

    Delete routes.js file (not necessary).

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+var express = require('express');
+var logger = require('morgan');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var app = express();
+var PORT = 3000;
+
+app.use(logger('dev'));
+
+app.get('/webpage/count', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
--- routes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/webpage/count', (req, res) => {
-  res.send('Hello World!');
-})
-
-module.exports = router;
