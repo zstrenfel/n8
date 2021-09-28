13e7d0c39804df6c065a0837074e43c38a9323ec
Zach Strenfel <zwstrenfel@gmail.com>
2021-09-27 20:23:03 -0400

    Create integration tests.

---
+++ tests/integration/app.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('POST webpage/count', () => {
+  it('should handle application/json requests"', async () => {
+    const response = await request(app)
+      .post('/webpage/count')
+      .type('application/json')
+      .send({ url: 'https://google.com' });
+    expect(response.statusCode).toEqual(200);
+    expect(Object.keys(response.body).length).toBeGreaterThan(0);
+  });
+
+  it('should handle application/x-www-form-urlencoded requests', async () => {
+    const response = await request(app)
+      .post('/webpage/count')
+      .type('application/x-www-form-urlencoded')
+      .send('url=https://google.com');
+    expect(response.statusCode).toEqual(200);
+    expect(Object.keys(response.body).length).toBeGreaterThan(0);
+  });
+
+  it('should count words for text/plain websites', async () => {
+    const response = await request(app)
+      .post('/webpage/count')
+      .type('application/json')
+      .send({ url: 'https://norvig.com/big.txt' });
+    expect(response.statusCode).toEqual(200);
+    expect(Object.keys(response.body).length).toBeGreaterThan(0);
+  });
+});
